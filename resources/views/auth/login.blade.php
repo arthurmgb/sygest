cc83f9da3f6249621d9a4fe06494ce9c1fb7bfb1
Arthur Oliveira <arthurpmgb@gmail.com>
2022-11-26 18:40:55 -0300

    Update login 26-11-22

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
                 <div class="flex items-center justify-between">
                     <x-jet-input placeholder="Digite sua senha" id="password" class="block mt-1 w-full primezze-input"
                         type="password" name="password" required autocomplete="off" />
-                    <div class="guest-toggle-pass ml-2">
+                    <div class="guest-toggle-pass ml-3">
                         <div class="btn-toggle-pass-visib" data-tooltip="Exibir" data-flow="top">
                             <i id="toggler-pass" style="font-family: 'Font Awesome 5 Pro' !important;" class="fad fa-eye fa-fw fa-lg"></i>
                         </div>
